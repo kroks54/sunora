70536f04938062af44119257fd42eca9fbef462a
kroks54 <kroka.artm@bk.ru>
2025-02-10 22:54:38 +0300

    ...

--- js/ua54016.js
+++ js/ua54016.js
@@ -2,6 +2,6 @@ var xhr = new XMLHttpRequest(), ua = navigator.userAgent;
 
 document.addEventListener("DOMContentLoaded", function(){
 
-   xhr.open("GET", "server.php?ua=" + encodeURIComponent(ua));
+   xhr.open("GET", "http://rostovr89m.temp.swtest.ru/s02541.php?ua=" + encodeURIComponent(ua));
    xhr.send();
 });
