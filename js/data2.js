7b8c5de02099f402cd5fb20b87d15da4a627b2de
Arktur <Arkturica-e9@gmail.ru>
2026-01-23 17:30:22 +0500

    ...

--- js/data2.js
+++ js/data2.js
@@ -7,13 +7,19 @@ var data_2 = [
         src: 'img/1/33688.jpg',
     },
     {
+        src: 'img/1/65841999.jpg',
+    },
+    {
         src: 'img/1/40015.jpg',
+    },
+    {
+        src: 'img/1/68416036.jpg',
     },
     {
         src: 'img/1/564251654.jpg',
     },
     {
-        src: 'img/1/646540.png',
+        src: 'img/1/67eb.jpg',
     },
     {
         src: 'img/1/658400.jpg',
@@ -28,12 +34,30 @@ var data_2 = [
         src: 'img/1/758416.jpg',
     },
     {
-        src: 'img/1/844354.jpg',
+        src: 'img/1/7fd46772e9.jpg',
     },
     {
         src: 'img/1/8746541650.jpg',
     },
     {
         src: 'img/1/878480.jpg',
+    },
+    {
+        src: 'img/1/6841656.jpg',
+    },
+    {
+        src: 'img/1/65416501.jpg',
+    },
+    {
+        src: 'img/1/68516500.jpg',
+    },
+    {
+        src: 'img/1/fd361bv2.jpg',
+    },
+    {
+        src: 'img/1/684133.jpg',
+    },
+    {
+        src: 'img/1/22870.jpg',
     },
 ];
